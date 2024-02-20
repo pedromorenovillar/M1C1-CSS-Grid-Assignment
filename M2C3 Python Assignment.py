05603022278f11d8f9cc9c83cac36eb555f02335
Pedro José Moreno Villar <151030573+pedromorenovillar@users.noreply.github.com>
2024-02-20 16:08:52 +0100

    ordered files of m1c2 and created file of python assignment

---
+++ M2C3 Python Assignment.py
@@ -0,0 +1,32 @@
+# Exercise 1
+my_string = 'Donostia is the capital of Gipuzkoa'
+my_number = 31
+my_list = [7, 8, 9, 10]
+my_boolean = False
+
+# Exercise 2
+my_string_initial_letters = my_string[:3]
+
+# Exercise 3
+my_list_first_element = my_list[0]
+
+# Exercise 4
+my_new_number = my_number + 10
+
+# Exercise 5
+my_list_last_element = my_list[-1]
+
+# Exercise 6
+names = 'harry,alex,susie,jared,gail,conner'
+split_names = names.split(',')
+
+# Exercise 7
+my_string_first_word = my_string[:my_string.index(' ')]
+my_string_first_word_uppercase = my_string_first_word.upper()
+my_new_string = my_string_first_word_uppercase + my_string[8:]
+
+# Exercise 8
+print(f'My cousin turns {my_number} this year')
+
+# Exercise 9
+print('hello world')
