dfa5690df4dd1b9ae189ef6e7938c7ef3161545c
Pedro José Moreno Villar <151030573+pedromorenovillar@users.noreply.github.com>
2024-04-04 10:20:42 +0200

    image added

--- M4C7/M4C7 JavaScript Assignment.js
+++ M4C7/M4C7 JavaScript Assignment.js
@@ -6,25 +6,29 @@ Si es más pequeño, la consola registra "¡El número es menor que 50!"
 */
 
 function myFunction (arg1, arg2, arg3, arg4) {
-
-    if (typeof arg1, arg2, arg3, arg4 !== 'number') {
+    var arg1 = arg1 || 0;
+    var arg2 = arg2 || 0;
+    var arg3 = arg3 || 0;
+    var arg4 = arg4 || 0;
+    
+    if (typeof arg1 !== 'number' || typeof arg2 !== 'number' || typeof arg3 !== 'number' || typeof arg4 !== 'number') {
         console.log('Solo se admiten números')
-    }
-
-    result = (arg1 + arg2) * (arg3 + arg4)
-    console.log(result)
-    if (result > 50) {
-        console.log('¡El número es mayor que 50!')
-    }
-    else if (result < 50) {
-        console.log('¡El número es menor que 50!')
-    }
-    else if (result === 50) {
-        console.log('¡El número es 50!')
+    } else {
+        result = (arg1 + arg2) * (arg3 + arg4)
+        console.log(result)
+        if (result > 50) {
+            console.log('¡El número es mayor que 50!')
+        }
+        else if (result < 50) {
+            console.log('¡El número es menor que 50!')
+        }
+        else if (result === 50) {
+            console.log('¡El número es 50!')
+        }
     }
 }
 
 myFunction(1,2,3,4)
 myFunction(5,5,5,5)
 myFunction(1,4,1,9)
-myFunction(1,'b',1,9)
+myFunction("papa",1,3)
