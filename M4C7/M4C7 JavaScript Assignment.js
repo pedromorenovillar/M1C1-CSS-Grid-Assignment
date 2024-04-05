72d80d2e2f6e540ccd1b5d529588d62695390889
Pedro José Moreno Villar <151030573+pedromorenovillar@users.noreply.github.com>
2024-04-05 12:47:23 +0200

    Update M4C7 JavaScript Assignment.js

--- M4C7/M4C7 JavaScript Assignment.js
+++ M4C7/M4C7 JavaScript Assignment.js
@@ -5,33 +5,29 @@ Si el número creado es mayor que 50, la consola registra "¡El número es mayor
 Si es más pequeño, la consola registra "¡El número es menor que 50!"
 */
 
-function myFunction (arg1, arg2, arg3, arg4) {
-    var arg1 = arg1 || 0;
-    var arg2 = arg2 || 0;
-    var arg3 = arg3 || 0;
-    var arg4 = arg4 || 0;
-    
-    if (typeof arg1 !== 'number' ||
-        typeof arg2 !== 'number' ||
-        typeof arg3 !== 'number' ||
-        typeof arg4 !== 'number') {
-        console.log('Solo se admiten números')
-    } else {
-        result = (arg1 + arg2) * (arg3 + arg4)
-        console.log(result)
-        if (result > 50) {
-            console.log('¡El número es mayor que 50!')
-        }
-        else if (result < 50) {
-            console.log('¡El número es menor que 50!')
-        }
-        else if (result === 50) {
-            console.log('¡El número es 50!')
+function sumaMultiplicacion (arg1, arg2, arg3, arg4) {
+    if (arguments.length !== 4) {
+        throw new Error('Es necesario utilizar 4 argumentos en esta función')
+      } else {
+        if (typeof arg1 !== 'number' ||
+            typeof arg2 !== 'number' ||
+            typeof arg3 !== 'number' ||
+            typeof arg4 !== 'number') {
+            throw new Error('Esta función solo admite números')
+        } else {
+            result = (arg1 + arg2) * (arg3 + arg4)
+            if (result > 50) {
+                console.log('¡El número es mayor que 50!')
+            }
+            else if (result < 50) {
+                console.log('¡El número es menor que 50!')
+            }
+            else if (result === 50) {
+                console.log('¡El número es 50!')
+            }
+            else {
+                throw new Error('Se ha producido un error'); 
+            }
         }
     }
 }
-
-myFunction(1,2,3,4)
-myFunction(5,5,5,5)
-myFunction(1,4,1,9)
-myFunction("papa",1,3)
